5b0bc4553b853e6f5f1e8b98d41425cd922a3f04
robbfox <foxy@lovecat.com>
2023-11-24 05:53:17 +0000

    removed Gatsby links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,7 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-    © <a href="https://www.rfoxy.com/">Robb Fox</a> 2023{' '}
-      </a>
+    © <a href="https://www.rfoxy.com">Robb Fox</a> 2023{' '}
     </div>
   </Container>
 )
