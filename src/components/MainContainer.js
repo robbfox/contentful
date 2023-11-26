66bf99dd199a7fb27212806bf521c0703f803793
robbfox <foxy@lovecat.com>
2023-11-26 19:01:34 +0000

    resolve access management

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -12,15 +12,18 @@ const MainContainer = () => {
         <color attach="background" args={['#000117']} />
         <AnimatedStars />
         <OrbitControls />
+        <>
           <Html position={[0, 0, 0]}>
-        <h1 className="title has-text-white has-text-centered">
+            <h1 className="title has-text-white has-text-centered">
               <Typing />
-          </h1>
-          <a href="/">
-            <button className="button is-success" style={{position: 'absolute', top: -340, left: -735 }}>Home</button>
-          </a>
-        
-        </Html>
+            </h1>
+            <a href="/">
+              <button className="button is-success" style={{ position: 'absolute', top: -340, left: -735 }}>
+                Home
+              </button>
+            </a>
+          </Html>
+        </>
       </Canvas>
     </div>
   );
