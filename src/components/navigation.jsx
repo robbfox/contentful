e69e3eb20b36ff2b55114de4afd88e20fad1e863
robbfox <foxy@lovecat.com>
2023-12-09 06:06:44 +0000

    fix ios navbar sticking

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -7,19 +7,25 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
+        <div onclick="">
         <Link to="/" activeClassName="active">
           Home
         </Link>
+        </div>
       </li>
       <li className={styles.navigationItem}>
+        <div onclick="">
         <Link to="/blog/" activeClassName="active">
           Blog
         </Link>
+        </div>
       </li>
       <li className={styles.navigationItem}>
+        <div onclick="">
         <Link to="/about/" activeClassName="active">
           About
         </Link>
+        </div>
       </li>
     </ul>
   </nav>
