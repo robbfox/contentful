90014611d6d1ab36935e2156595cd532d0f9562c
robbfox <foxy@lovecat.com>
2023-12-14 13:55:52 +0000

    add layout to 'about'page

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -7,24 +7,26 @@ import DesktopTyping from './DesktopTyping';
 import MediaQuery from 'react-responsive';
 import MobileTyping from './MobileTyping';
 import Navigation from './navigation';
+import './MainContainer_styling.css'
+
 
 const MainContainer = () => {
  
   return (
-    <div style={{ height: '100vh', width: '100vw' }}>
+    <div style={{ height: '80vh', width: '90vw', margin: 'auto'}}>
       <Canvas>
         <color attach="background" args={['#000117']} />
         <AnimatedStars />
         <OrbitControls />
         <>
-        <Html position={[0, 0, 0]}>
-          <h1 className="title has-text-white has-text-centered">
+  
+        <Html  position={[0, 0, 0]}>
+        <h1 className="title has-text-white has-text-centered">
               <MediaQuery minWidth={1224}><DesktopTyping /></MediaQuery>
               <MediaQuery maxWidth={1224}><MobileTyping /></MediaQuery>
               </h1>
-            
-             </Html>
-        </>
+           </Html>
+  </>
       </Canvas>
     </div>
     
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import MainContainer from '../components/MainContainer'
+import Layout from '../components/layout'
+import { motion } from 'framer-motion'
 class AboutIndex extends React.Component {
   render() {
      return (
+      <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}>
+   <Layout>
     <MainContainer />
-          )
+    </Layout>
+    </motion.div>
+ )
   } 
 }
 export default AboutIndex
