7e25475cc58ae0693d1dc615a1666587f41672b4
robbfox <foxy@lovecat.com>
2023-12-27 18:45:33 +0000

    moved about-text to contentful

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,97 @@
-import React from 'react'
-import MainContainer from '../components/MainContainer'
-import Layout from '../components/layout'
-import { motion } from 'framer-motion'
+import React from 'react';
+import Layout from '../components/layout';
+import { motion } from 'framer-motion';
+import { Canvas } from '@react-three/fiber';
+import { Html, OrbitControls } from '@react-three/drei';
+import AnimatedStars from '../components/AnimatedStars';
+import '../components/bulma/css/bulma.css';
+import MediaQuery from 'react-responsive';
+import '../components/MainContainer_styling.css';
+import ReactTyped from 'react-typed-component';
+import { graphql } from 'gatsby';
+import { get } from 'lodash';
+import '../components/DesktopTyping.css';
+import '../components/MobileTyping.css';
+import TypeWriterEffect from 'react-typewriter-effect';
 
-const AboutIndex = () => {
-    return (
-      <motion.div
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      transition={{ duration: 1.5 }}>
-   <Layout>
-    <MainContainer />
-    </Layout>
-    </motion.div>
- )
-  } 
 
-  export default AboutIndex
+
+// ... (other imports)
+
+const AboutIndex = ({ data }) => {
+   const stuff = get(data, 'allContentfulPerson.nodes[0].about.raw', '');
+   const parsedStuff = JSON.parse(stuff);
+   const textContent = parsedStuff.content[0].content[0].value;
+ 
+   return (
+     <motion.div
+       animate={{ opacity: 1 }}
+       initial={{ opacity: 0 }}
+       transition={{ duration: 1.5 }}
+     >
+       <Layout>
+         <div style={{ height: '80vh', width: '90vw', margin: 'auto' }}>
+           <Canvas>
+             <color attach="background" args={['#000117']} />
+             <AnimatedStars />
+             <OrbitControls />
+             <>
+               <Html position={[-6, 1, 0]}>
+                 <MediaQuery minWidth={1224}>
+                   <h1 className="title has-text-white has-text-centered">
+                     <div
+                       className="desktoptyping-container desktop-typing-text"
+                       style={{ display: 'inline-block' }}
+                     >
+                       <ReactTyped
+                         strings={[textContent]}
+                         typeSpeed={40}
+                         backspeed={60}
+                         loop
+                       />
+                     </div>
+                   </h1>
+                 </MediaQuery>
+                 <MediaQuery maxWidth={1224}>
+                   <h1 className="title has-text-white has-text-centered">
+                   <div
+                       className="mobile-typing-container mobile-typing-text"
+                       style={{ display: 'inline-block' }}
+                     >
+                       
+                       <ReactTyped
+                         strings={[textContent]}
+                         typeSpeed={40}
+                         backspeed={60}
+                         loop
+                       />
+                     </div>
+                   </h1>
+                 </MediaQuery>
+               </Html>
+             </>
+           </Canvas>
+         </div>
+       </Layout>
+     </motion.div>
+   );
+ };
+ 
+ export default AboutIndex;
+ 
+ // ... (remaining code)
+ 
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    allContentfulPerson(
+      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
+    ) {
+      nodes {
+        about {
+          raw
+        }
+      }
+    }
+  }
+`;
