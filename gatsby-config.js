98259b8f0e8fe3dab89c4b19690e0e6afc8e9d1b
robbfox <foxy@lovecat.com>
2024-01-01 13:20:11 +0000

    add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
-
 module.exports = {
   siteMetadata: {
     title: "Robb Fox",
@@ -18,6 +17,19 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         host: process.env.CONTENTFUL_HOST,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "./favicon.png", // This path is relative to the root of the site.
+        crossOrigin: `use-credentials`,
       },
     },
   ],
