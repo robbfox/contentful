edd6ad406d7149c2449df257a0555ab402752a49
robbfox <robbfox@outlook.com>
2025-04-19 11:11:02 +0100

    update year

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-    © <a href="https://www.deviantart.com/robbfox">Robb Fox</a> 2023{' '}
+    © <a href="https://www.deviantart.com/robbfox">Robb Fox</a> 2025{' '}
     </div>
   </Container>
 )
