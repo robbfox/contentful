410dbb9d349f94f018433fb4d03bca506fc3dbf4
Robert Fox <75976583+robbfox@users.noreply.github.com>
2026-06-24 09:25:34 +0000

    Refactor BlogIndex component for improved pagination and query parameters

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,42 +7,37 @@ import Hero from '../components/hero';
 import ArticlePreview from '../components/article-preview';
 import '../components/global.css'
 
+const BlogIndex = ({ location, pageContext, data }) => {
+  const posts = get(data, 'allContentfulBlogPost.nodes');
+  const { currentPage, numPages } = pageContext;
 
+  return (
+    <Layout location={location}>
+      <Seo title={`Blog - Page ${currentPage}`} />
+      <Hero title="Blog" />
+      <ArticlePreview posts={posts} />
+      <div>
+        {Array.from({ length: numPages }).map((_, i) => (
+          <Link
+            key={`pagination-number${i + 1}`}
+            to={`/blog/${i + 1}`}
+          >
+            {i + 1}
+          </Link>
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
-
-
-
-const BlogIndex = ({location, pageContext, data}) => {
-    const posts = get(data, 'allContentfulBlogPost.nodes');
-    const { currentPage, numPages } = pageContext;
-
-    return (
-      <Layout location={location}>
-          <Seo title={`Blog - Page ${currentPage}`} />
-          <Hero title="Blog" />
-          <ArticlePreview posts={posts} />
-          <div>
-            {Array.from({ length: numPages }).map((_, i) => (
-              <Link
-                key={`pagination-number${i + 1}`}
-                to={i === -1 ? '/blog/1' : `/blog/${i + 1}`}
-              >
-                {i + 1}
-              </Link>
-            ))}
-          </div>
-        </Layout>
-      );
-  }
-
-
-  export default BlogIndex;
+export default BlogIndex;
 
 export const pageQuery = graphql`
-  query BlogIndexQuery {
+  query BlogIndexQuery($skip: Int!, $limit: Int!) {
     allContentfulBlogPost(
       sort: { publishDate: DESC }
-      limit: 6
+      limit: $limit
+      skip: $skip
     ) {
       nodes {
         title
